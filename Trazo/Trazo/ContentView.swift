d86a4ddb3b1f576baa840fb7929d0304765b1798
Pablo Zapata <saldana_d@outlook.com>
2026-06-12 00:30:45 -0600

    Trazo Files

---
+++ Trazo/Trazo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trazo
+//
+//  Created by Diego Saldaña on 12/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trazo/Trazo/TrazoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrazoApp.swift
+//  Trazo
+//
+//  Created by Diego Saldaña on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TrazoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
